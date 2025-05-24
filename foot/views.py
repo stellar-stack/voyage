17ad93aafd657cfbecc6b81431a3c2de4dfdfddb
strack <elliotgould5721@gmail.com>
2025-05-24 18:54:03 +0530

    inital voyage commit

---
+++ foot/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def Home(request):
+    return render(request, 'home.html')
+
+def Room(request):
+    return  render(request, 'room.html')
